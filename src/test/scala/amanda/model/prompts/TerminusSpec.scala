e6b531d52a9f58b4d4c9044174bdf7e76af98fa7
Mo Moledina <mo.moledina@txodds.com>
2019-09-06 16:02:13 +0100

    Added TerminusSpec

---
+++ src/test/scala/amanda/model/prompts/TerminusSpec.scala
@@ -0,0 +1,15 @@
+package amanda.model.prompts
+
+import org.specs2.mutable.Specification
+
+class TerminusSpec extends Specification {
+
+  "Terminus" should {
+
+    "" in {
+      success
+    }.mute
+
+  }
+
+}
