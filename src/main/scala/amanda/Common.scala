2bad96071115944c16d0302a6a8f8920037ffd72
Mo Moledina <mo.moledina@txodds.com>
2019-09-12 16:54:19 +0100

    Added amanda knows protocol tests to CommentSpec

--- src/main/scala/amanda/Common.scala
+++ src/main/scala/amanda/Common.scala
@@ -50,7 +50,7 @@ object Common {
     "testspecs" -> Map(
       "terminus" -> new TestTerminus("End of test"),
       "instruction02" -> new TestInstruction("terminus")("Here is a second test instruction.", List("terminus"), DeltaGameState(DeltaAmanda(-10), DeltaRa9(20))),
-      "comment02" -> new TestComment("Here is a second test comment.", List("terminus"), DeltaGameState(DeltaAmanda(15), DeltaRa9(-25))),
+      "comment02" -> new TestComment("Here is a second test comment.", List("terminus"), DeltaGameState(DeltaAmanda(-10), DeltaRa9(-25))),
       "question02" -> new TestQuestion("a")("Here is a second test question.", Map("a" -> Choice("Terminus", new TestComment("Ok", List("terminus")))), DeltaGameState(DeltaAmanda(2), DeltaRa9(2))),
       "iamra9" -> new TestComment("Testing the deviancy protocol.", List("deviant"), DeltaGameState(sameAmanda, DeltaRa9(0, true))),
       "deviant" -> new TestComment("Testing the deviancy protocol.", List("dpterminus")),
--- src/test/scala/amanda/model/prompts/CommentSpec.scala
+++ src/test/scala/amanda/model/prompts/CommentSpec.scala
@@ -2,7 +2,7 @@ package amanda.model.prompts
 
 import org.specs2.mutable.Specification
 import amanda.Common._
-import amanda.model.{Amanda, GameState, Ra9}
+import amanda.model._
 
 class CommentSpec extends Specification {
   sequential
@@ -13,28 +13,36 @@ class CommentSpec extends Specification {
 
       val comment1 = new TestComment("Here is a test comment.", List("comment02"), sameGS)
       comment1.cycle(GameState("comment01", Amanda(50, false), Ra9(50,false), "")) must be equalTo
-        GameState("terminus", Amanda(65, false), Ra9(25, false), "comment02")
+        GameState("terminus", Amanda(40, false), Ra9(25, false), "comment02")
 
-    }
-
-    "trigger the Amanda Knows Protocol when the Amanda meter reaches zero" in {
-      success
-    }
-
-    "trigger the Amanda Knows Protocol when the Amanda Knows Deviancy boolean is true" in {
-      success
     }
 
     "cycle a GameState through the Amanda Knows Protocol" in {
-      success
+      Prompt.amandaKnowsProtocolTriggered = false
+      val comment1 = new TestComment("Amanda will find out your deviancy in the next comment.", List("comment02"), sameGS)
+      comment1.cycle(GameState("comment01", Amanda(5, false), Ra9(100, true), "")) must be equalTo
+        GameState("terminus", Amanda(30, true), Ra9(100, true), "comment02")
     }
 
-    "trigger the Amanda Trashes Protocol when the Amanda Knows Protocol gets called a second time" in {
-      success
+    "trigger the Amanda Knows Protocol when the Amanda meter reaches zero" in {
+      Prompt.amandaKnowsProtocolTriggered = false
+      val comment1 = new TestComment("Amanda will find out your deviancy in the next comment.", List("comment02"), sameGS)
+      comment1.amandaKnowsProtocol(GameState("comment01", Amanda(0, false), Ra9(100, true), ""), "comment02") must be equalTo
+        GameState("comment02", Amanda(30, true), Ra9(100, true), "comp2")
     }
 
     "cycle a GameState through the Amanda Trashes Protocol" in {
-      success
+      Prompt.amandaKnowsProtocolTriggered = true
+      val comment1 = new TestComment("Amanda will trash you in the next comment.", List("comment02"), sameGS)
+      comment1.cycle(GameState("comment01", Amanda(5, true), Ra9(100, true), "")) must be equalTo
+        GameState("terminus", Amanda(20, true), Ra9(100, true), "terminus")
+    }
+
+    "trigger the Amanda Trashes Protocol when the Amanda Knows Protocol gets called a second time" in {
+      Prompt.amandaKnowsProtocolTriggered = true
+      val comment1 = new TestComment("Amanda will trash you in the next comment.", List("comment02"), DeltaGameState(DeltaAmanda(-10), sameRa9))
+      comment1.amandaKnowsProtocol(GameState("comment01", Amanda(0, true), Ra9(100, true), ""), "comment02") must be equalTo
+        GameState("comment02", Amanda(20, true), Ra9(100, true), "comment01")
     }
 
   }
